36dd5b61a98a43cb2622f1f4a430169d8f3619ef
Brian Cochran <bcochran23@cornellcollege.edu>
2022-05-18 14:17:29 +0000

    Removed unneeded data

--- clean_data.r
+++ clean_data.r
@@ -9,9 +9,10 @@ names(data)[c(12,14)] <- c("NO3_N", "E_coli")
 NBear <- c("North Bear", "N. Bear", "N Bear", "NBear")
 LC <- c("Tile", "Lime240", "Lime250", "Lime290", "LimeFin", "LimeHam", "Lime230", "Lime270", "Lime270E", "Lime270W", "Lime280E", "Lime280W", "Lime295", "Lime310", "LimeFrost", "Lime 240", "Lime 250", "Lime 290", "Lime Fin", "Lime Ham")
 IC <- c("Dry", "ICLM", "ICS", "ICThom", "ICN")
-MC <- c("MCJ", "MC42", "MC100", "MCKenn", "MCNoel")
+MR <- c("MCJ", "MC42", "MC100", "MCKenn", "MCNoel")
+no_include <- c("BF1", "BF2", "BF3", "BF4", "Pond")
 
-# Create watershed column and fix date column
+# Create watershed column, fix date column, remove unneeded data
 data <- data[-3703,] %>%
   mutate(Watershed = Site) %>%
   mutate(Watershed = if_else(Watershed %in% NBear, "North Bear", Watershed)) %>%
@@ -19,7 +20,8 @@ data <- data[-3703,] %>%
   mutate(Watershed = if_else(Watershed %in% LC, "Lime", Watershed)) %>%
   mutate(Watershed = if_else(Watershed %in% IC, "Indian Creek", Watershed))%>%
   mutate(Watershed = if_else(Watershed=="Otter 2", "Otter", Watershed)) %>%
-  mutate(Watershed = if_else(Watershed %in% MC, "Morgan", Watershed)) %>%
+  mutate(Watershed = if_else(Watershed %in% MR, "McLoud Run", Watershed)) %>%
+  filter(!(Site %in% no_include)) %>%
   mutate(Date = as.Date(Date, origin = "1899-12-30")) %>%
   relocate(Watershed, .before=Site)
 
